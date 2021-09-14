a673b33a95c72194b576c2d5958872c5dc75f0d7
caitlin-rich <caitlinboagrich@gmail.com>
2021-09-14 13:01:18 -0400

    moves index.html to root

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "..", "public/index.html"))
+  res.sendFile(path.join(__dirname, "..", "index.html"))
 );
 
 // static file-serving middleware
